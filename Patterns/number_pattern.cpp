9fbf4e91530b4b88611d22d5d3fca28926c0d5fc
farhanniamatali171-creator <farhanniamatali171@gmail.com>
2026-08-21 10:01:30 +0500

    Create number_pattern.cpp

---
+++ Patterns/number_pattern.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cstdlib> 
+
+using namespace std;
+
+int main(){
+  
+int n=4;
+int m= 4;
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <=m; j++)
+    {
+    cout<< j;
+    }
+    cout<<endl;
+    
+  }
+  
+    system("pause"); 
+    
+    return 0;
+}
