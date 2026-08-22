b8ecc85c300bbce5bee9f606e8cb4bfee5214fa2
farhanniamatali171-creator <farhanniamatali171@gmail.com>
2026-08-22 09:57:51 +0500

    Create character_square_pattern.cpp

---
+++ Patterns/character_square_pattern.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cstdlib> 
+
+using namespace std;
+
+int main(){
+  
+int n=4;
+int m= 4;
+  for (int i = 0; i < n; i++)
+  {
+    char ch='A';
+    for (int j = 0; j <m; j++)
+    {
+    cout<< ch;
+    ch+=1;
+    }
+    cout<<endl;
+    
+  }
+  
+    system("pause"); 
+    
+    return 0;
+}
