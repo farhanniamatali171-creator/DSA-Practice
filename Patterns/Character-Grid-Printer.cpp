2a54796f7f50aad810ba1cab1381bf75aebadf38
farhanniamatali171-creator <farhanniamatali171@gmail.com>
2026-08-24 10:02:12 +0500

    Create Character-Grid-Printer.cpp

---
+++ Patterns/Character-Grid-Printer.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cstdlib> 
+
+using namespace std;
+
+int main(){
+  
+int n=3;
+int m= 3;
+  char ch='A'; 
+  for (int i = 0; i < n; i++)
+  {
+  
+    for (int j = 0; j < n; j++)
+    {
+    cout<< ch;
+   ch++;
+    
+    }
+    cout<<endl;
+    
+  }
+  
+  
+    system("pause"); 
+    
+    return 0;
+}
