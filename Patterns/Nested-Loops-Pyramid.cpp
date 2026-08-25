93348be262f22ea1b9196fb28bb698496834788e
farhanniamatali171-creator <farhanniamatali171@gmail.com>
2026-08-25 11:22:49 +0500

    Create Nested-Loops-Pyramid.cpp

---
+++ Patterns/Nested-Loops-Pyramid.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cstdlib> 
+
+using namespace std;
+
+int main(){
+  
+int n=3;
+int m= 3;
+
+  for (int i = 0; i <= n; i++)
+  {
+  
+    for (int j = 0; j <= i; j++)
+    {
+    cout<< "*";
+ 
+    
+    }
+    cout<<endl;
+    
+  }
+  
+  
+    system("pause"); 
+    
+    return 0;
+}
