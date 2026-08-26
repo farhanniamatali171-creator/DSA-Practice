1fb42a205224a4968d66fe957547dbb4b7fb991e
farhanniamatali171-creator <farhanniamatali171@gmail.com>
2026-08-26 09:55:02 +0500

    Create Repeating-Number-Pyramid.cpp

---
+++ Patterns/Repeating-Number-Pyramid.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cstdlib> 
+
+using namespace std;
+
+int main(){
+  
+int n=4;
+
+
+  for (int i = 1; i <= n; i++)
+  {
+  int m= 3;
+    for (int j = 1; j <= i; j++)
+    {
+    cout<< i;
+ 
+    
+    }
+    cout<<endl;
+    
+  }
+  
+  
+    system("pause"); 
+    
+    return 0;
+}
+title
